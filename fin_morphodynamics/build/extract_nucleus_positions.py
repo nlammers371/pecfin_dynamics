2ebede2bf8f933329f3345ec666b13c2881b37b3
Nicholas Lammers <nlammers@berkeley.edu>
2023-09-19 20:02:41 -0700

    Continuing work on script to extrac nucleus mask positions

--- fin_morphodynamics/build/extract_nucleus_positions.py
+++ fin_morphodynamics/build/extract_nucleus_positions.py
@@ -1,6 +1,6 @@
 # import shutil
 # from ome_zarr.reader import Reader
-# from tqdm import tqdm
+from tqdm import tqdm
 import pandas as pd
 import numpy as np
 import glob2 as glob
@@ -10,63 +10,61 @@ from aicsimageio import AICSImage
 # import math
 # import dask
 import os
+from functions.utilities import path_leaf
 # import open3d as o3d
 # import fractal_tasks_core
 
 
-def extract_nucleus_stats_prob(data_root, date_folder, voxel_res=1):
+def extract_nucleus_stats_prob(data_root, date_folder, voxel_res=1, prob_thresh=0):
 
     # get paths to raw data and processed probability stacks
     raw_root = os.path.join(data_root, "raw_data", date_folder, "")
     built_root = os.path.join(data_root, "built_data", date_folder, "")
+    nucleus_root = os.path.join(data_root, "nucleus_data", date_folder, "")
+    if not os.path.isdir(nucleus_root):
+        os.makedirs(nucleus_root)
 
     # get metadata from raw file. If there arte multiple .nd2 files in single date folder, we assume that the pixel
     # sizes are the same for each
     nd2_paths = glob.glob(raw_root + "*.nd2")
     imObject = AICSImage(nd2_paths[0])
+
+    # extract dimension info and generate a scale reference array
     pixel_res_raw = np.asarray(imObject.physical_pixel_sizes)
+    im_dims = np.asarray([imObject.dims["Z"][0], imObject.dims["Y"][0], imObject.dims["X"][0]])
+
+    z_vec = np.arange(im_dims[0]) * pixel_res_raw[0]
+    y_vec = np.arange(im_dims[1]) * pixel_res_raw[1]
+    x_vec = np.arange(im_dims[2]) * pixel_res_raw[2]
+
+    z_ref_array, y_ref_array, x_ref_array = np.meshgrid(z_vec, y_vec, x_vec, indexing="ij")
 
     # get list of datasets with nucleus labels
-    image_list = sorted(glob.glob(built_root + "*_prob.tif"))
+    image_list = sorted(glob.glob(built_root + "*_probs.tif"))
 
-    for im in tqdm(range(len(image_list))):
+    for im, prob_path in enumerate(tqdm(image_list)):
 
-        labelPath = image_list[im]
-        labelName = image_list[im].replace(dataRoot, '', 1)
-        cellLabelPath = labelPath + "Cell/"
-        labelName = labelName.replace('.zarrlabels', '')
-        rawImagePath = rawRoot + labelName[:-2] + "/" + labelName + ".czi"  # have to draw from raw data for now...
-        imagePath = dataRoot + labelName + ".zarr"
-        savePath = dataRoot + labelName + '_nucleus_props.csv'
+        # extract metadata
+        prob_name = path_leaf(prob_path)
+        data_name = prob_name.replace("_prob.tif", "")
+        save_path = os.path.join(nucleus_root, data_name, '_nucleus_props.csv')
 
-        resetClass = (not os.path.isfile(savePath)) | resetClassGB
-        overwriteCellLabels = (not os.path.isdir(cellLabelPath)) | overwriteCellLabelsGB
+        # well number
+        well_ind = prob_name.find("well")
+        well_num = int(prob_name[well_ind+4:well_ind+7])
 
-        print('Extracting stats for ' + labelName)
-        reader = Reader(parse_url(imagePath))
-        #
-        # # nodes may include images, labels etc
-        nodes = list(reader())
-        #
-        # # first node will be the image pixel data
-        image_node = nodes[0]
-        image_data = image_node.data
+        # time step index
+        time_ind = int(prob_name[well_ind + 9:well_ind+12])
 
-        #############
-        # Labels
-        #############
+        print('Extracting stats for ' + data_name)
+        probImage = AICSImage(prob_path)
+        prob_data = probImage.data
+        prob_data_thresh = prob_data > prob_thresh
+        nc_indices = np.where(prob_data_thresh == 1)[0]
 
-        # read the image data
-        # store_lb = parse_url(labelPath, mode="r").store
-        reader_lb = Reader(parse_url(labelPath))
-
-        # nodes may include images, labels etc
-        nodes_lb = list(reader_lb())
-
-        # first node will be the image pixel data
-        label_node = nodes_lb[1]
-        label_data = label_node.data
-
+        nc_z = z_ref_array[nc_indices]
+        nc_y = y_ref_array[nc_indices]
+        nc_x = x_ref_array[nc_indices]
         # extract key image attributes
         # omero_attrs = image_node.root.zarr.root_attrs['omero']
         # channel_metadata = omero_attrs['channels']  # list of channels and relevant info
@@ -240,4 +238,5 @@ if __name__ == "__main__":
     # define some variables
     root = "/Users/nick/Dropbox (Cole Trapnell's Lab)/Nick/pecfin_dynamics/fin_morphodynamics/"
     date_folder = "20230830"
-    extract_nucleus_stats_prob(root, date_folder)
+    prob_thresh = -8
+    extract_nucleus_stats_prob(root, date_folder, prob_thresh=prob_thresh)
