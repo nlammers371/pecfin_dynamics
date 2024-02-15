f651f88e2b612ea9159dfa90fda3c791d87888b0
nlammers371 <nlammers@berkeley.edu>
2024-02-15 12:44:36 -0800

    Work ongoing to incporporate segmentation model into curation script

--- fin_morphodynamics/src/build/pecfin_curation_v3.py
+++ fin_morphodynamics/src/build/pecfin_curation_v3.py
@@ -4,14 +4,17 @@ import napari
 import os
 from glob2 import glob
 import skimage.io as skio
-from alphashape import alphashape
-from functions.utilities import path_leaf
-from skimage.transform import resize
 import pandas as pd
 # import open3d as o3d
+from joblib import dump, load
+from sklearn.neural_network import MLPClassifier
+import time
+
 
 
 def on_points_click(layer, event):
+    global train_df, mdl
+
     if event.type == 'mouse_press' and event.button == 1 and layer.mode == "select":  # Left mouse button
         # selected_index = layer.get_value(event.position, world=True)
         # print(f"Selected point index: {selected_index}")
@@ -45,6 +48,40 @@ def on_points_click(layer, event):
                 point_features.iloc[selected_index, 0] = 4
                 point_layer.features = point_features
 
+
+            # update data frames
+            start = time.time()
+            point_df.loc[:, "fin_label_cur"] = point_layer.features.copy() - 1
+
+            train_df_temp = point_df.loc[point_df["fin_label_cur"] != -1]
+            train_df_temp.loc[:, "well_index"] = well_index
+            train_df_temp.loc[:, "time_index"] = time_index
+            # train_df_temp.loc[:, global_df.columns.tolist()] = np.tile(global_df.iloc[:, :].to_numpy(),
+            #                                                            (train_df_temp.shape[0], 1)).copy()
+
+            train_orig = train_df.copy()
+            train_df = pd.concat([train_orig, train_df_temp], axis=0)
+            train_df = train_df[~train_df.index.duplicated(keep='first')]
+
+            # fit the classifier
+            feature_cols = [c for c in train_df.columns if "feat" in c]
+            X_train = train_df.loc[:, feature_cols]
+            Y_train = train_df.loc[:, "fin_label_cur"]
+            mdl = mdl.fit(X_train, Y_train)
+
+            # get new predictions
+            X_pd = point_df.loc[:, feature_cols]
+            Y_pd = mdl.predict(X_pd)
+            print(time.time() - start)
+
+            # Update pd layer features
+            # pd_features = pd_layer.features.copy()
+            # pd_features = Y_pd + 1
+            pd_features = pd_layer.features.copy()
+            pd_features.iloc[:, 0] = Y_pd + 1
+            pd_layer.features = pd_features
+
+            # pd_layer.loc[:, "fin_label_pd"] = Y_pd.copy()
         else:
             pass
 
@@ -52,71 +89,42 @@ def on_points_click(layer, event):
         return
 
 
-# 
-# def calculate_fin_hull(point_data, hull_alpha=4):
-# 
-#     # point_data = point_data.source.data
-#     point_data_norm = np.divide(point_data, im_dims)
-#     update_lb_flag = False
-#     if point_data.shape[0] == 6:
-#         update_lb_flag = True
-#         
-#         hull = alphashape(point_data_norm, alpha=hull_alpha)
-#         values = np.linspace(0.5, 1, len(hull.vertices))
-#         surf = (np.multiply(hull.vertices, im_dims), hull.faces, values)
-#         fin_hull_layer = viewer.add_surface(surf, name="fin_hull", colormap="bop blue", scale=scale_vec, opacity=0.5)
-# 
-#     elif point_data.shape[0] > 6:
-#         update_lb_flag = True
-#         
-#         hull = alphashape(point_data_norm, alpha=hull_alpha)
-#         values = np.linspace(0.5, 1, len(hull.vertices))
-#         surf = (np.multiply(hull.vertices, im_dims), hull.faces, values)
-# 
-#         if "fin_hull" not in viewer.layers:
-#             fin_hull_layer = viewer.add_surface(surf, name="fin_hull", colormap="bop blue", scale=scale_vec,
-#                                                 opacity=0.5)
-#         viewer.layers["fin_hull"].data = surf
-#         viewer.layers["fin_hull"].refresh()
-#     
-#     if update_lb_flag:
-#         if len(hull.faces) > 2:
-#             inside_flags = hull.contains(zyx_nuclei_norm)
-#             fin_points = zyx_nuclei[np.where(inside_flags == 1)[0], :].astype(int)
-#             new_labels = np.zeros(im_dims_ds, dtype=np.int8)
-#             new_labels[fin_points[:, 0], fin_points[:, 1], fin_points[:, 2]] = 1
-#             viewer.layers["labels"].data = resize(new_labels, im_dims, preserve_range=True, order=0)
-#             viewer.layers["labels"].refresh()
+def curate_pec_fins(root, experiment_date, overwrite_flag=False, n_mlp_nodes=500):
+    global train_df, mdl
 
-def curate_pec_fins(root, experiment_date, overwrite_flag=False):
-    
     # load metadata
     metadata_path = os.path.join(root, "metadata", experiment_date + "_master_metadata_df.csv")
     metadata_df = pd.read_csv(metadata_path, index_col=0)
-    
+
+    # check for previously-trained models and curation data
+    mdl_path = os.path.join(root, "metadata",  experiment_date + "_MLP_mdl.joblib")
+    curated_date_path = os.path.join(root, "metadata", experiment_date + "_MLP_data.csv")
+    if os.path.isfile(mdl_path):
+        mdl = load(mdl_path)
+    else:
+        mdl = MLPClassifier(random_state=1, max_iter=5000, hidden_layer_sizes=n_mlp_nodes)
+    if os.path.isfile(curated_date_path):
+        train_df = pd.read_csv(curated_date_path, index_col=0)
+    else:
+        train_df = []
+
     # add column to track fin segmentation
     if "fin_curation_flag" not in metadata_df.columns:
         metadata_df["fin_curation_flag"] = False
     if "fin_curation_date" not in metadata_df:
         metadata_df["fin_curation_date"] = np.nan
-    
-    # load raw image
-    # ref_image_path = glob(os.path.join(root, "raw_data", experiment_date, "*.nd2"))
-    # if len(ref_image_path) > 1:
-    #     raise Exception("Multiple nd2 files found.")
-    #
-    # imObject = AICSImage(ref_image_path[0])
+
     
     # git list of prob files produced by CellPose
     prob_file_dir = os.path.join(root, "built_data", "cellpose_output", experiment_date, "")
     
     # make directory for saving fin masks
-    fin_point_dir = os.path.join(root, "built_data", "point_clouds", experiment_date, "")
+    fin_point_dir = os.path.join(root, "built_data", "processed_point_clouds", experiment_date, "")
     
     # Extract pixel sizes and bit_depth
     pixel_res_vec = metadata_df.loc[0, ["z_res_um", "y_res_um", "x_res_um"]].to_numpy().astype(np.float32)
     scale_vec = tuple(pixel_res_vec)
-    scale_array = np.asarray(scale_vec)
+    # scale_array = np.asarray(scale_vec)
     
     # get list of fins to segment
     if not overwrite_flag:
@@ -130,17 +138,26 @@ def curate_pec_fins(root, experiment_date, overwrite_flag=False):
     image_i = 0
 
     while image_i < len(well_id_list):
-        
-        fname_suffix = f"well{well_id_list[image_i]:03}_t{time_id_list[image_i]:03}"
-        
+
+        global curation_df, point_layer, point_df, global_df, time_index, well_index, pd_layer
+
+        well_index = well_id_list[image_i]
+        time_index = time_id_list[image_i]
+        fname_suffix = f"well{well_index:03}_t{time_index:03}"
+
         # load point cloud
         point_cloud_options = glob(os.path.join(fin_point_dir, "*" + fname_suffix + "*"))
-        if len(point_cloud_options) > 1:
+        local_options = [p for p in point_cloud_options if "global" not in p]
+        global_options = [p for p in point_cloud_options if "global" in p]
+        if (len(local_options) > 1) | (len(global_options) > 1):
             raise Exception("Found multiple files with same well and time index.")
-        point_df = pd.read_csv(point_cloud_options[0], index_col=0)
+        point_df = pd.read_csv(local_options[0], index_col=0)
+        global_df = pd.read_csv(global_options[0], index_col=0)
+        point_df.loc[:, global_df.columns.tolist()] = np.tile(global_df.iloc[:, :].to_numpy(),
+                                                                   (point_df.shape[0], 1)).copy()
         if "fin_label_cur" not in point_df.columns:
             point_df["fin_label_cur"] = -1
-
+        # point_df["fin_label_pd"] = -1
         # load probability map from cellpose
         prob_file_list = glob(prob_file_dir + "*" + fname_suffix + "_probs*")
         if len(prob_file_list) > 1:
@@ -149,8 +166,7 @@ def curate_pec_fins(root, experiment_date, overwrite_flag=False):
 
         # generate temporary DF to keep track of curation labels
         curation_df = pd.DataFrame(point_df.loc[:, "fin_label_cur"].copy())
-
-
+        curation_df["fin_label_pd"] = -1
         # 0=outlier, 1=fin, 2=yolk, 3=body
         curation_df["outlier_flags"] = curation_df["fin_label_cur"] == 0
         curation_df["fin_flags"] = curation_df["fin_label_cur"] == 1
@@ -159,14 +175,26 @@ def curate_pec_fins(root, experiment_date, overwrite_flag=False):
 
         curation_df.loc[:, "fin_label_cur"] += 1  # for labeling convenience
 
+        # generate wide feature DF for classifier training
+        train_df_temp = point_df.loc[point_df["fin_label_cur"] != -1]
+        train_df_temp.loc[:, "well_index"] = well_index
+        train_df_temp.loc[:, "time_index"] = time_index
+        # train_df_temp.loc[:, global_df.columns.tolist()] = np.tile(global_df.iloc[:, :].to_numpy(), (train_df_temp.shape[0], 1)).copy()
+        if len(train_df) > 0:
+            train_df = pd.concat([train_df, train_df_temp], axis=0)
+            train_df = train_df.drop_duplicates(ignore_index=True)
+        else:
+            train_df = train_df_temp.copy()
+
+        # initialize model if it doesn't already exist
+
         # set colormap
         label_color_cycle = ["white", "gray", "green", "red", "blue"]
-
-        global point_layer
 
         # initialize viewer
         viewer = napari.view_image(im_prob, colormap="gray", scale=scale_vec,
                                    contrast_limits=(point_df.loc[0, "prob_thresh"], np.percentile(im_prob, 99.8)))
+
 
         # # generate master point array to integrate results
         point_layer = viewer.add_points(point_df.loc[:, ["Z", "Y", "X"]].to_numpy(), name='point labels',
@@ -192,6 +220,11 @@ def curate_pec_fins(root, experiment_date, overwrite_flag=False):
                                        face_color="body_flags",
                                        face_color_cycle=[label_color_cycle[0]] + [label_color_cycle[4]], visible=False)
 
+        pd_layer = viewer.add_points(point_df.loc[:, ["Z", "Y", "X"]].to_numpy(), name='predictions',
+                                        size=4, features=curation_df.loc[:, "fin_label_pd"],
+                                        face_color="fin_label_pd", opacity=0.7,
+                                        face_color_cycle=label_color_cycle, visible=True)
+
         # connect to event trigger function
         outlier_layer.mouse_drag_callbacks.append(on_points_click)
         fin_layer.mouse_drag_callbacks.append(on_points_click)
@@ -213,12 +246,13 @@ def curate_pec_fins(root, experiment_date, overwrite_flag=False):
         # points_layer = viewer.add_points(point_array, name="fin hull points", size=8, scale=scale_vec, n_dimensional=True)
         #
         # points_layer.events.data.connect(event_trigger)
+        # point_df.loc[:, "fin_label_cur"] = point_layer.features.copy() - 1
 
         napari.run()
 
         print("Saving...")
-        point_df.loc[:, "fin_label_cur"] = point_layer.features.copy() - 1
-        point_df.to_csv(point_cloud_options[0])
+
+        # point_df.to_csv(local_options[0])
 
 
         wait = input(
