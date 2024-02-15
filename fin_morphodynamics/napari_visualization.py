d2d396edbe5239dc02898bbcd1f44cc1a4ffcdee
Nicholas Lammers <nlammers@berkeley.edu>
2024-02-14 16:29:15 -0800

    Minor tweeks to visualization code

--- fin_morphodynamics/napari_visualization.py
+++ fin_morphodynamics/napari_visualization.py
@@ -8,30 +8,22 @@ import os
 from ome_zarr.io import parse_url
 
 
-root = "E:\\Nick\\Cole Trapnell's Lab Dropbox\\Nick Lammers\\Nick\pecfin_dynamics\\fin_morphodynamics\\"
-experiment_date = "20231013"
+root = "/Users/nick/Cole Trapnell's Lab Dropbox/Nick Lammers/Nick/pecfin_dynamics/fin_morphodynamics/raw_data/HCR/"
+experiment_date = "20240112"
 
-nd2_list = glob.glob(os.path.join())
-full_filename = "/Users/nick/Dropbox (Cole Trapnell's Lab)/Nick/pecfin_dynamics/fin_morphodynamics/raw_data/20230830/tdTom_54hpf_pecfin_40x.nd2"
+file_list = sorted(glob.glob(os.path.join(root, experiment_date, "") + "*.nd2"))
+full_filename = file_list[4]
+print(full_filename)
 imObject = AICSImage(full_filename)
-imObject.set_scene("XYPos:6")
-imData = np.squeeze(imObject.get_image_data("TZYX", C=0))
+# imObject.set_scene("XYPos:6")
+imData = np.squeeze(imObject.data)
 
 # Extract pixel sizes and bit_depth
 res_raw = imObject.physical_pixel_sizes
 scale_vec = np.asarray(res_raw)
+print(scale_vec)
 
-shape_curr = imData.shape
-rs_factor = scale_vec[0] / scale_vec[1]
-shape_new = np.asarray(shape_curr)
-shape_new[0] = np.round(shape_new[0]*rs_factor).astype(int)
-# shape_new = np.round(shape_new / 2).astype(int) # downsize by a factor of 2
-imData_rs = resize(imData, shape_new, order=1, anti_aliasing=False)
-#
-# # with open("/Users/nick/RNA300_GFP_10x_wholeEmbryo.npy", 'wb') as f:
-# # np.save("/Users/nick/RNA300_GFP_10x_wholeEmbryo.npy", imData)
-#
-viewer = napari.view_image(imData_rs, colormap="magenta")#, scale=res_array)
+viewer = napari.view_image(imData, channel_axis=0, scale=scale_vec)#, scale=res_array)
 
 # # labels_layer = viewer.add_labels(lbData, name='segmentation', scale=res_array)
 if __name__ == '__main__':
