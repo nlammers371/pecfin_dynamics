4717ab0c1ec7c442fb0ca1368516bb3eeaef3674
nlammers371 <nlammers@t001.grid.gs.washington.edu>
2024-07-18 17:51:28 -0700

    Fixed path variables

--- results/20240718/segment_nuclei_20240711.py
+++ results/20240718/segment_nuclei_20240711.py
@@ -5,14 +5,15 @@ cell_diameter = 10
 cellprob_threshold = 0.0
 
 # set path to CellPose model to use
-# pretrained_model0 = "/media/nick/hdd02/Cole Trapnell's Lab Dropbox/Nick Lammers/Nick/pecfin_dynamics/built_data/cellpose_training/standard_models/tdTom-bright-log-v5"
+pretrained_model0 = "/media/nick/hdd02/Cole Trapnell's Lab Dropbox/Nick Lammers/Nick/pecfin_dynamics/built_data/cellpose_training/standard_models/tdTom-bright-log-v5"
 # pretrained_model1 = "/media/nick/hdd02/Cole Trapnell's Lab Dropbox/Nick Lammers/Nick/pecfin_dynamics/built_data/cellpose_training/standard_models/tdTom-dim-log-v3"
-pretrained_model0 = "Y:\\data\\pecfin_dynamics\\built_data\\cellpose_training\\standard_models\\tdTom-bright-log-v5"
+# pretrained_model0 = "Y:\\data\\pecfin_dynamics\\built_data\\cellpose_training\\standard_models\\tdTom-bright-log-v5"
 # pretrained_model1 = "Y:\\data\\pecfin_dynamics\\built_data\\cellpose_training\\standard_models\\tdTom-dim-log-v3"
 
 # set read/write paths
 # root = "/media/nick/hdd02/Cole Trapnell's Lab Dropbox/Nick Lammers/Nick/pecfin_dynamics/"
-root = "Y:\\data\\pecfin_dynamics\\" #"/net/trapnell/vol1/home/nlammers/projects/data/pecfin_dynamics/"
+# root = "Y:\\data\\pecfin_dynamics\\" 
+root = "/net/trapnell/vol1/home/nlammers/projects/data/pecfin_dynamics/"
 experiment_date_vec = ["20240711_01", "20240711_02", "20240712_01", "20240712_02"] #, "20240425", "20240619", "20240620"]
 pretrained_model_vec = [pretrained_model0, pretrained_model0, pretrained_model0, pretrained_model0] #[pretrained_model0, pretrained_model1, pretrained_model1, pretrained_model0, pretrained_model0]
 nuclear_channel_vec = [0, 0, 0, 0] #[0, 1, 1, 0, 0]
