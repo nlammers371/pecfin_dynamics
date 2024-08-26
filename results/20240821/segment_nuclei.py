cf4f4c24a0acac5a2c4bcc02613941fd592e9ad5
nlammers371 <nlammers@berkeley.edu>
2024-08-26 11:34:35 -0700

    Improved metadata extraction and zarr export to handle wider range of file types

---
+++ results/20240821/segment_nuclei.py
@@ -0,0 +1,28 @@
+from src.build.build01_segment_nuclei_zarr import cellpose_segmentation
+
+# use XX environment
+
+overwrite = False
+cell_diameter = 10
+cellprob_threshold = 0.0
+
+# set path to CellPose model to use
+# pretrained_model0 = "/net/trapnell/vol1/home/nlammers/projects/data/pecfin_dynamics/built_data/cellpose_training/standard_models/tdTom-bright-log-v5"
+# pretrained_model1 = "/media/nick/hdd02/Cole Trapnell's Lab Dropbox/Nick Lammers/Nick/pecfin_dynamics/built_data/cellpose_training/standard_models/tdTom-dim-log-v3"
+pretrained_model0 = "Y:\\data\\pecfin_dynamics\\built_data\\cellpose_training\\standard_models\\tdTom-bright-log-v5"
+# pretrained_model1 = "Y:\\data\\pecfin_dynamics\\built_data\\cellpose_training\\standard_models\\tdTom-dim-log-v3"
+
+# set read/write paths
+# root = "/media/nick/hdd02/Cole Trapnell's Lab Dropbox/Nick Lammers/Nick/pecfin_dynamics/"
+root = "Y:\\data\\pecfin_dynamics\\"
+# root = "/net/trapnell/vol1/home/nlammers/projects/data/pecfin_dynamics/"
+experiment_date_vec = ["20240726"] #, "20240712_01", "20240712_02"] #, "20240425", "20240619", "20240620"]
+pretrained_model_vec = [pretrained_model0] #, pretrained_model0, pretrained_model0] #[pretrained_model0, pretrained_model1, pretrained_model1, pretrained_model0, pretrained_model0]
+nuclear_channel_vec = [1, 1] #, 0, 0] #[0, 1, 1, 0, 0]
+well_list = None
+for e, experiment_date in enumerate(experiment_date_vec):
+    pretrained_model = pretrained_model_vec[e]
+    nuclear_channel = nuclear_channel_vec[e]
+    cellpose_segmentation(root=root, experiment_date=experiment_date, well_list=well_list,
+                          xy_ds_factor=1, cell_diameter=cell_diameter, nuclear_channel=nuclear_channel,
+                          cellprob_threshold=cellprob_threshold, pretrained_model=pretrained_model, overwrite=overwrite)
--- visualization/napari_visualize_temp.py
+++ visualization/napari_visualize_temp.py
@@ -12,9 +12,9 @@ import zarr
 
 
 # set paths
-mask_path = "E:/Nick/Cole Trapnell's Lab Dropbox/Nick Lammers/Nick/pecfin_dynamics/fin_morphodynamics/built_data\stitched_labels\log-v5/20240223/20240223_well0012_labels_stitched.zarr"
+mask_path = "Y:/data/pecfin_dynamics/built_data/zarr_image_files/20240725/20240725_well0000.zarr"
 mask_zarr = zarr.open(mask_path, mode="r")
-time_ind = 2
+time_ind = 0
 scale_vec = tuple([2.0, 0.55, 0.55])
 
 
@@ -22,7 +22,7 @@ scale_vec = tuple([2.0, 0.55, 0.55])
 viewer = napari.Viewer(ndisplay=3)
 
 # viewer.add_labels(im_label1, scale=scale_vec, name="bkg")
-viewer.add_labels(mask_zarr[time_ind], scale=scale_vec)
+viewer.add_image(mask_zarr[:, time_ind], scale=scale_vec, contrast_limits=[0, 2**16])
 # viewer.add_labels(stitched_mask, name="aff labels")
 # viewer.add_labels(stitched_mask_p6, name="aff labels p6")
 # viewer.add_labels(stitched_mask_n6, name="aff labels n6")
