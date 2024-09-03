df33cc7b5eb70a672d0316ef8f209f685250de2b
nlammers371 <nlammers@berkeley.edu>
2024-09-03 16:35:08 -0700

    Updates to export script for increased flexibility

--- visualization/napari_visualize_temp.py
+++ visualization/napari_visualize_temp.py
@@ -9,26 +9,18 @@ from skimage.morphology import label
 from skimage.measure import regionprops
 import zarr
 
-
-
 # set paths
-mask_path = "Y:/data/pecfin_dynamics/built_data/zarr_image_files/20240725/20240725_well0000.zarr"
+mask_path = "Y:/data/pecfin_dynamics/built_data/zarr_image_files/20240726/20240726_well0010.zarr"
 mask_zarr = zarr.open(mask_path, mode="r")
 time_ind = 0
 scale_vec = tuple([2.0, 0.55, 0.55])
-
 
 # generate napari viewer instance
 viewer = napari.Viewer(ndisplay=3)
 
 # viewer.add_labels(im_label1, scale=scale_vec, name="bkg")
-viewer.add_image(mask_zarr[:, time_ind], scale=scale_vec, contrast_limits=[0, 2**16])
-# viewer.add_labels(stitched_mask, name="aff labels")
-# viewer.add_labels(stitched_mask_p6, name="aff labels p6")
-# viewer.add_labels(stitched_mask_n6, name="aff labels n6")
+viewer.add_image(mask_zarr[:, time_ind], scale=scale_vec, channel_axis=0, colormap=["Green", "magenta"], name=["fli-GFP", "H2B-tdTom"], contrast_limits=[0, 2**16])
 
-# viewer.add_labels(label(im_prob > -4), scale=scale_vec)
+viewer.window.add_plugin_dock_widget(plugin_name='napari-animation')
 
-
-# if __name__ == '__main__':
-#     napari.run()
+napari.run()
